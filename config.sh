21a682de608bccec958a8a1c54d9274d564e17af
akme <akme@users.noreply.github.com>
2018-08-18 23:55:43 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,14 +1,11 @@
-tag="rds" #cluster name
-numberofnodes="3" #cluster size
+tag="rds" #cluster name, bid's and tasks will use it
+numberofnodes="3" # cluster size
 #node config
-ramsize="3" # GB
-storagesize="20" #GB
-cpucores="2" #number of cores
+ramsize="3" # GB, integers only
+storagesize="20" # GB, integers only
+cpucores="2" #number of cores, integers only
 sysbenchsingle="500"
 sysbenchmulti="600"
-netdownload="20" # Mbits
-netupload="20" # Mbits
+netdownload="20" # Mbits, integers only
+netupload="20" # Mbits, integers only
 price="0.005" # $ per hour
-
-
-
